fb0466d469cede57b8be45c60f2528780c040e13
davi894 <moisesdavi005@gmail.com>
2023-01-11 10:58:21 -0300

    Add: configurando deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,43 @@
 import "dotenv/config";
 import path from "path";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+const nodeEnv: string = process.env.NODE_ENV;
+
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
+  const migrationsPath: string = path.join(__dirname, "./migrations/*.{js,ts}");
+
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.PGHOST,
+    port: parseInt(process.env.PGPORT!),
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database:  process.env.PGDATABASE,
+    logging: false,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+export const AppDataSource = new DataSource(dataSourceConfig());
+
+
+
+
+/* import "dotenv/config";
+import path from "path";
 import { DataSource } from "typeorm";
 
 const AppDataSource = new DataSource(
@@ -25,3 +63,4 @@ const AppDataSource = new DataSource(
 );
 
 export default AppDataSource;
+ */
