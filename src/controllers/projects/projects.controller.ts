7e36cb1a2e1de6dd1a84008ca48063f03d99cf39
TawanAlencar <josetawan300@gmail.com>
2023-01-12 14:33:22 -0300

    Add: list total one project

--- src/controllers/projects/projects.controller.ts
+++ src/controllers/projects/projects.controller.ts
@@ -1,6 +1,7 @@
 import { createProjectsService } from "../../services/projects/create.projects.services"
 import { Request,Response } from "express"
 import { listProjectsService } from "../../services/projects/list.projects.services"
+import { listTotalOneProjectsService } from "../../services/projects/list.totalOneProject.services"
 
 
 export const createProjectsController = async (request:Request, response:Response)=>{
@@ -10,5 +11,10 @@ export const createProjectsController = async (request:Request, response:Respons
 
 export const listProjectsController = async (request:Request, response:Response)=>{
     const [status,json] = await listProjectsService()
+    return response.status(status as number).json(json)
+}
+
+export const listTotalOneProjectController = async (request:Request, response:Response)=>{
+    const [status,json] = await listTotalOneProjectsService()
     return response.status(status as number).json(json)
 }
--- src/routes/projects/projects.routes.ts
+++ src/routes/projects/projects.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createProjectsController, listProjectsController } from "../../controllers/projects/projects.controller";
+import { createProjectsController, listProjectsController, listTotalOneProjectController } from "../../controllers/projects/projects.controller";
 
 
 export const projectsRouter = Router()
 
 projectsRouter.post("/",createProjectsController)
 projectsRouter.get("/",listProjectsController)
-projectsRouter.get("/:project_id/total")
+projectsRouter.get("/:project_id/total",listTotalOneProjectController)
