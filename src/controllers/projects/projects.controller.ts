d84b222b964c51ec826b3f0f50f7d7d35bd288a4
TawanAlencar <josetawan300@gmail.com>
2023-01-12 12:33:40 -0300

    Add: list all projects

--- src/controllers/projects/projects.controller.ts
+++ src/controllers/projects/projects.controller.ts
@@ -1,8 +1,14 @@
 import { createProjectsService } from "../../services/projects/create.projects.services"
 import { Request,Response } from "express"
+import { listProjectsService } from "../../services/projects/list.projects.services"
 
 
 export const createProjectsController = async (request:Request, response:Response)=>{
     const [status,json] = await createProjectsService(request.body)
+    return response.status(status as number).json(json)
+}
+
+export const listProjectsController = async (request:Request, response:Response)=>{
+    const [status,json] = await listProjectsService()
     return response.status(status as number).json(json)
 }
--- src/routes/projects/projects.routes.ts
+++ src/routes/projects/projects.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createProjectsController } from "../../controllers/projects/projects.controller";
+import { createProjectsController, listProjectsController } from "../../controllers/projects/projects.controller";
 
 
 export const projectsRouter = Router()
 
 projectsRouter.post("/",createProjectsController)
+projectsRouter.get("/",listProjectsController)
