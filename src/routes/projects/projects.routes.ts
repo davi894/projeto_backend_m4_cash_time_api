5f6ecc7b4b2889873b5d60008084670b4bd1522d
TawanAlencar <josetawan300@gmail.com>
2023-01-13 11:52:19 -0300

    Fix: resolve conflits

--- src/routes/projects/projects.routes.ts
+++ src/routes/projects/projects.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { createProjectsController, deleteProjectsController, getOneProjectController, getTotalAllProjectsController, listProjectsController, listTotalOneProjectController, updateProjectsController } from "../../controllers/projects/projects.controller";
+import { createProjectsController, getOneProjectController, getTotalAllProjectsController, listProjectsController, listTotalOneProjectController, updateProjectsController } from "../../controllers/projects/projects.controller";
 import { verifyErrorMiddleware } from "../../middleware/verifyError.middleware";
 import { projectSchema, updateProjectsSchema } from "../../schemas/projects.schemas";
 
@@ -13,4 +13,3 @@ projectsRouter.get("/:project_id/total",listTotalOneProjectController)
 projectsRouter.get("/total",getTotalAllProjectsController)
 projectsRouter.get("/:id",getOneProjectController)
 projectsRouter.patch("/:id",verifyErrorMiddleware(updateProjectsSchema),updateProjectsController)
-projectsRouter.delete("/:id",deleteProjectsController)
