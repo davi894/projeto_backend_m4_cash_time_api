78efabb4d7431dcf0620af5bb36ff170251a40d6
davi894 <moisesdavi005@gmail.com>
2023-01-16 07:16:28 -0300

    Fix: entidade checkpoint column date para o type date

--- src/entities/checkpoint.ts
+++ src/entities/checkpoint.ts
@@ -1,31 +1,25 @@
-import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 
 import { User } from "./user";
 import { Projects } from "./projects";
 
 @Entity("checkpoint")
 export class Checkpoint {
-    @PrimaryGeneratedColumn("uuid")
-    id: string
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column({type:"time"})
-    entry: string
+  @Column({ type: "time" })
+  entry: string;
 
-    @Column({type:"time"})
-    output: string
+  @Column({ type: "time" })
+  output: string;
 
-    @Column({type:"time"})
-    date: string
+  @Column({ type: "date" })
+  date: string;
 
-    @ManyToOne(() => User,(u) => u.id )
-    user_: User
+  @ManyToOne(() => User, (u) => u.id)
+  user_: User;
 
-    @ManyToOne(()=> Projects, (p) => p.id)
-    projects_: Projects
+  @ManyToOne(() => Projects, (p) => p.id)
+  projects_: Projects;
 }
-  
